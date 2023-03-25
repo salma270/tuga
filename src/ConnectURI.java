787df31f40dde5af58e7d0c0c1983db4b9adc553
salma270 <putsallut@gmail.com>
2023-03-25 16:49:31 +0700

    salma270

---
+++ src/ConnectURI.java
@@ -0,0 +1,40 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ConnectURI {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static URL buildURL(String urlQuery) {
+        URL url = null;
+        try {
+            url = new URL(urlQuery.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter(Pattern.compile("//A"));
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
+
---
+++ src/Farmasi.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Farmasi {
+    private JPanel PANEL1;
+    private JButton SUBMITButton;
+    private JButton closeButton;
+    private JTextField textField1;
+    private JTextField textField2;
+}
